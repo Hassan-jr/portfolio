9168b05efb857c31e745bbb02b2e06e1d78bcaf5
Hassan-jr <abdiladifhassan115@gmail.com>
2022-07-29 13:47:30 +0300

    body nav updated

--- src/components/Bodynav.js
+++ src/components/Bodynav.js
@@ -1,29 +1,28 @@
-import React from 'react'
-import {Link} from "react-router-dom";
-import "./Bodynav.css"
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Bodynav.css";
 
 function Bodynav() {
   return (
-          <div id='resume'>
-                  <h2 className='resume'>RESUME</h2>
-    <div className='bodynav' >
+    <div id="resume">
+      <h2 className="resume">RESUME</h2>
+      <div className="bodynav">
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <h5>WORK HISTORY</h5>
+        </Link>
+        <Link to="/skills" style={{ textDecoration: "none" }}>
+          <h5>SKILLS</h5>
+        </Link>
 
-       <Link  to ="/"  style={{ textDecoration: 'none' }}>
-           <h5>WORK HISTORY</h5>
+        <Link to="/interest" style={{ textDecoration: "none" }}>
+          <h5>INTERESTS</h5>
         </Link>
-        <Link to ="/skills"  style={{ textDecoration: 'none' }}>
-         <h5>SKILLS</h5>
+        <Link to="/edu" style={{ textDecoration: "none" }}>
+          <h5>EDUCATION BACKGROUND</h5>
         </Link>
-        
-        <Link  to ="/interest"  style={{ textDecoration: 'none' }}>
-<h5>INTERESTS</h5>
-        </Link>
-        <Link  to ="/edu"  style={{ textDecoration: 'none' }}>
-<h5>EDUCATION BACKGROUND</h5>
-        </Link>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default Bodynav
+export default Bodynav;
