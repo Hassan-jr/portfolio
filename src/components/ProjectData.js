55c08dae52f985d70188bab7ccc8b37e38d55755
Hassan-jr <abdiladifhassan115@gmail.com>
2022-08-27 10:38:46 +0300

    Projects Updated

--- src/components/ProjectData.js
+++ src/components/ProjectData.js
@@ -1,28 +1,33 @@
 import Image1 from "../img/amazon.png";
 import Image2 from "../img/dashboard1.png";
 import Image3 from "../img/caske3.png";
+import Image4 from "../img/blog.png";
 const Data = [
-    
-        {   
-            title: '1. Responsive Amazon-clone Website',
-            link :  "http://amazonbyhassan.herokuapp.com/",
-            image: Image1,
-            hosting: "Heroku",
-        },
-        {   
-            title: '2. Admin Dashboard',
-            link :  "https://github.com/Hassan-jr/Admin-Dashboard",
-            image: Image2,
-            hosting: "GitHub",
-        },
-        {   
-            title: '3. Real Estate Project',
-            link :  "https://github.com/Hassan-jr/Caske",
-            image: Image3,
-            hosting: "GitHub",
-        },
-        
-    
+  {
+    title: "1. Blog Website",
+    link: "https://hassanjrblog.herokuapp.com/",
+    image: Image4,
+    hosting: "Heroku",
+  },
+
+  {
+    title: "2. Responsive Amazon-clone Website",
+    link: "http://amazonbyhassan.herokuapp.com/",
+    image: Image1,
+    hosting: "Heroku",
+  },
+  {
+    title: "3. Admin Dashboard",
+    link: "https://github.com/Hassan-jr/Admin-Dashboard",
+    image: Image2,
+    hosting: "GitHub",
+  },
+  {
+    title: "4. Real Estate Project",
+    link: "https://github.com/Hassan-jr/Caske",
+    image: Image3,
+    hosting: "GitHub",
+  },
 ];
 
 export default Data;
